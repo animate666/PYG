293b344dbb4739032c0e3f8ace644b2c0cda8b9f
Daildel <animate_y@163.com>
2019-04-03 17:12:58 +0800

    轮播图渲染完成

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
-$(function(){
-    // 动态生成轮播图结构
+/* 轮播图 */
+$(function () {
+
     $.ajax({
-        type:'get',
-        url:'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
-        dataType:'json',
-        success:function(result){
-            console.log(result)
-            if(result.meta.status == 200){
-                // 只有获取数据成功了，才有必要生成动态结构
-                // 生成图片结构
-                var html = template('pyg_bannerTemp',result)
-                console.log(html)
-                $('.pyg_indexbanner').html(html)
-                // 生成点标记结构
-                var indiHTML = template('indicatorTemp',result)
-                $('.mui-slider-indicator').html(indiHTML)
-                mui('.mui-slider').slider({
-                    interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
-                });
+        type: 'get',
+        url: 'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
+        datatype: 'json',
+        success: function (result) {
+            // console.log(result);
+            // 获取数据成功得到响应
+            if (result.meta.status == 200) {
+                var html = template('sliderTemp', result)
+                $('.pyg_banner').html(html)
             }
+            //获得slider插件对象  轮播图自动轮播
+            var gallery = mui('.mui-slider');
+            gallery.slider({
+                interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
+            });
         }
     })
 })
